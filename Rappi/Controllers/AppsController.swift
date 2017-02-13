48eeee45e07e410bf92e612b293d18b2f71c4771
Santiago Restrepo <santiagorestrepo@MacBook-Air-de-Santiago.local>
2017-02-13 10:10:37 -0500

    Creating controllers

---
+++ Rappi/Controllers/AppsController.swift
@@ -0,0 +1,17 @@
+//
+//  AppsController.swift
+//  Rappi
+//
+//  Created by Santiago Restrepo on 13/02/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+import UIKit
+
+class AppsController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ Rappi/Controllers/CategoriesController.swift
@@ -0,0 +1,17 @@
+//
+//  CategoriesController.swift
+//  Rappi
+//
+//  Created by Santiago Restrepo on 13/02/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
